0287c8d165031691afe30c7b1066e9e7669ef9fd
atesfay <abrahamkt@yahoo.com>
2019-10-30 11:09:47 -0700

    Add All layout and componenets

--- Scope/Scope/src/App.js
+++ Scope/Scope/src/App.js
@@ -1,36 +1,74 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
-import Header from './components/layout/Header'
-import Navigation from './components/layout/Navigation'
-import Canvas from './components/layout/Canvas'
-import About from './components/pages/About'
+// import React, {Component} from 'react';
+// import {BrowserRouter as Router, Route } from 'react-router-dom';
+// import './App.css';
+// import Header from './components/layout/Header'
+// import Navigation from './components/layout/Navigation'
+// import Canvas from './components/layout/Canvas'
+// import About from './components/pages/About'
 
 
 
 
-class App extends Component {
+// class App extends Component {
 
+//   render() {
+//     return (
+//       // If you use Router, you need to wrap everything in the Router
+//       <Router>
+//         <div className="App">
+//           <div className ="navigation_container">
+//             <Navigation/>
+//           </div>
+//           <div className="container">
+//             <Header />
+//             <Route exact path= "/"  render = {props => (
+//               <Canvas/>
+//             )}
+//             />
+//             <Route path ="/About" component = {About}/>
+//           </div>
+//         </div>
+//       </Router>
+//           );
+//           }
+//           }
+
+// export default App;
+
+
+import React, { Component } from 'react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Chart } from 'react-chartjs-2';
+import { ThemeProvider } from '@material-ui/styles';
+import validate from 'validate.js';
+
+import { chartjs } from './helpers';
+import theme from './theme';
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import './assets/scss/index.scss';
+import validators from './common/validators';
+import Routes from './Routes';
+
+const browserHistory = createBrowserHistory();
+
+Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
+  draw: chartjs.draw
+});
+
+validate.validators = {
+  ...validate.validators,
+  ...validators
+};
+
+export default class App extends Component {
   render() {
     return (
-      // If you use Router, you need to wrap everything in the Router
-      <Router>
-        <div className="App">
-          <div className ="navigation_container">
-            <Navigation/>
-          </div>
-          <div className="container">
-            <Header />
-            <Route exact path= "/"  render = {props => (
-              <Canvas/>
-            )}
-            />
-            <Route path ="/About" component = {About}/>
-          </div>
-        </div>
-      </Router>
-          );
-          }
-          }
-
-export default App;
+      <ThemeProvider theme={theme}>
+        <Router history={browserHistory}>
+          <Routes />
+        </Router>
+      </ThemeProvider>
+    );
+  }
+}
