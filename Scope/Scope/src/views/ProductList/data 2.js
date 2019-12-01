43e964bd753ef70e17803ad71d56e7586126770f
CindyLizette <cindylc728@gmail.com>
2019-11-30 19:29:28 -0800

    Project page edit

---
+++ Scope/Scope/src/views/ProductList/data 2.js
@@ -0,0 +1,36 @@
+import uuid from 'uuid/v1';
+
+export default [
+  {
+    project_title: "Scope",
+    project_description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum, inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    member_size: 3,
+    imageUrl: '/images/products/product_3.png',
+  },
+  {
+    project_title: "Angular",
+    project_description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum, inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    member_size: 2,
+    imageUrl: '/images/products/product_3.png',
+  }, {
+    project_title: "React",
+    project_description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum, inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    member_size: 3,
+    imageUrl: '/images/products/product_3.png',
+  }, {
+    project_title: "React-Native",
+    project_description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum, inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    member_size: 3,
+    imageUrl: '/images/products/product_3.png',
+  }, {
+    project_title: "Vue",
+    project_description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum, inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    member_size: 4,
+    imageUrl: '/images/products/product_3.png',
+  }, {
+    project_title: "CS157A",
+    project_description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum, inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. ",
+    member_size: 3,
+    imageUrl: '/images/products/product_3.png',
+  },
+];
