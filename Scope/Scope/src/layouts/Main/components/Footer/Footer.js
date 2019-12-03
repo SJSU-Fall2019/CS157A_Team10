e5bb282db22e3490c8577238235fc96ec2ea3cbe
Yulong Ran <yulongran@gmail.com>
2019-12-03 14:37:02 -0800

    Removed footer link

--- Scope/Scope/src/layouts/Main/components/Footer/Footer.js
+++ Scope/Scope/src/layouts/Main/components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = props => {
         &copy;{' '}
         <Link
           component="a"
-          href="https://devias.io/"
+          href="https://sjsu.edu/"
           target="_blank"
         >
           Scope
